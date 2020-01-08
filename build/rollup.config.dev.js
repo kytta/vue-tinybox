a402292f78e3c58901542f15bab12939ddc67416
Nikita Karamov <nick@karamoff.dev>
2020-01-09 00:33:59 +0100

    Add Nuxt.js dev environment
    Closes #13

--- build/rollup.config.dev.js
+++ build/rollup.config.dev.js
@@ -1,15 +1,29 @@
 import { defaultOutput, input } from './defaults';
 import { resolve, vue } from './plugins';
 
-export default {
-  input,
-  output: {
-    ...defaultOutput,
-    file: './dev/tinybox.js',
-    format: 'umd',
+export default [
+  {
+    input,
+    output: {
+      ...defaultOutput,
+      file: './dev/nuxt/components/tinybox.js',
+      format: 'esm',
+    },
+    plugins: [
+      resolve(),
+      vue(false),
+    ],
   },
-  plugins: [
-    resolve(),
-    vue(false),
-  ],
-};
+  {
+    input,
+    output: {
+      ...defaultOutput,
+      file: './dev/basic/tinybox.js',
+      format: 'umd',
+    },
+    plugins: [
+      resolve(),
+      vue(false),
+    ],
+  },
+];
---
+++ dev/nuxt/nuxt.config.js
@@ -0,0 +1,13 @@
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: '[DEV-NUXT] vue-tinybox',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+  },
+};

c73a6332ebcac9844fc1d89a900ec24f69797532
Nikita Karamov <nick@karamoff.dev>
2020-04-26 09:46:32 +0200

    Update wrapper code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import component from './tinybox.vue';
 
-function install(Vue) {
+// executed by Vue.use()
+const install = (Vue) => {
   if (install.installed) return;
   install.installed = true;
-
   Vue.component(component.name, component);
-}
+};
 
+
+// auto-install when Vue is found
 let GlobalVue = null;
-
 if (typeof window !== 'undefined') {
   GlobalVue = window.Vue;
 } else if (typeof global !== 'undefined') {
   GlobalVue = global.Vue;
 }
-
 if (GlobalVue) {
-  install(GlobalVue);
+  GlobalVue.use({ install });
 }
 
+// to allow module use
 export default component;

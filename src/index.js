59c32722c522d9fd740a18432ad82b605f318e5d
Nikita Karamov <me@kytta.dev>
2023-02-02 18:08:09 +0100

    Remove auto-install

--- src/index.js
+++ src/index.js
@@ -1,25 +1,3 @@
-import component from "./tinybox.vue";
+import Component from "./tinybox.vue";
 
-// Executed by Vue.use()
-const install = (Vue) => {
-	if (install.installed) return;
-	install.installed = true;
-	Vue.component(component.name, component);
-};
-
-// Auto-install when Vue is found
-let GlobalVue = null;
-if (typeof window !== "undefined") {
-	GlobalVue = window.Vue;
-	// eslint-disable-next-line no-undef
-} else if (typeof global !== "undefined") {
-	// eslint-disable-next-line no-undef
-	GlobalVue = global.Vue;
-}
-
-if (GlobalVue) {
-	GlobalVue.use({ install });
-}
-
-// To allow module use
-export default component;
+export default Component;

099896b48f926fb504bf1fd4ec2655a27bb8b5c9
Nikita Karamov <me@kytta.dev>
2023-02-02 15:08:52 +0100

    Do minor updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import { terser } from '@rollup/plugin-terser';
 import vue from 'rollup-plugin-vue';
 
 import autoprefixer from 'autoprefixer';

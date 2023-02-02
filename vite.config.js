fb215a66f3c3bc840e903dcbae818858ab4858b2
Nikita Karamov <me@kytta.dev>
2023-02-03 00:59:59 +0100

    Set up Vitest

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { resolve } from "node:path";
 
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 
 import autoprefixer from "autoprefixer";
 
 export default defineConfig({
+	test: {
+		environment: "jsdom",
+		root: "src"
+	},
 	root: resolve("./dev"),
 	build: {
 		lib: {

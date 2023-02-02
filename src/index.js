8d7b310a26d862d275a4332f641cb3efc2a18129
Nikita Karamov <me@kytta.dev>
2023-02-02 23:32:44 +0100

    Allow app.use()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import Component from "./tinybox.vue";
 
-export default Component;
+// Allow installing with app.use()
+// Source: https://github.com/houtaroy/vue-component-template/blob/619cacae5b000e4afdc623f2cb5f64fa522214ca/package/utils/ComponentUtil.ts
+const withInstall = (comp) => {
+	const c = comp;
+	c.install = function (app) {
+		app.component(c.name, Component);
+	};
+
+	return comp;
+};
+
+export default withInstall(Component);
